e073c15e1927cb2a309de370e572d59182ea87d0
gberto <gberto@fbk.eu>
2020-10-15 06:44:55 -0400

    change from tract names to mask names

--- run.py
+++ run.py
@@ -21,28 +21,28 @@ if __name__ == '__main__':
 	#Write results on a file
 	results = 'csv/output_FiberStats.csv'
 	metrics = ['FD']
-	first_row = ['tract_name']
+	first_row = ['bundle_mask_name']
 	first_row[1:len(metrics)] = metrics
 	with open(results, 'a') as csvFile:
 		writer = csv.writer(csvFile)
 		writer.writerow(first_row)
 
-	with open('tract_name_list.txt') as f:
-		tract_name_list = f.read().splitlines()
+	with open('mask_name_list.txt') as f:
+		mask_name_list = f.read().splitlines()
 
-	results_matrix = np.zeros((len(tract_name_list), len(metrics)))
+	results_matrix = np.zeros((len(mask_name_list), len(metrics)))
 	
-	for t, tract_name in enumerate(tract_name_list):
-		fname = '%s/%s.nii.gz' %(args.masks, tract_name)
+	for t, mask_name in enumerate(mask_name_list):
+		fname = '%s/%s' %(args.masks, mask_name)
 		vol_mask = nib.load(fname).get_data()
 		fractal_dimension, box_size, counts = compute_fractal_dimension(vol_mask,
                                                                     n_steps=None,
                                                                     box_size_max=None,
                                                                     box_size=box_size,
                                                                     verbose=False)
-		print("Fractal dimension of %s = %s" %(tract_name, fractal_dimension))
+		print("Fractal dimension of %s = %s" %(mask_name, fractal_dimension))
 		results_matrix[t] = [fractal_dimension] 
-		row = [tract_name]
+		row = [mask_name]
 		row[1:len(metrics)] = np.float16(results_matrix[t])
 		with open(results, 'a') as csvFile:
 			writer = csv.writer(csvFile)
