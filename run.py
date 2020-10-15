a94c2428394794ac5be98ce251e156ff29f9e52b
gberto <gberto@fbk.eu>
2020-10-15 06:23:02 -0400

    update

---
+++ run.py
@@ -0,0 +1,51 @@
+import sys
+import argparse
+import csv
+import numpy as np
+import nibabel as nib
+from fractal_dimension import compute_fractal_dimension
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-masks', nargs='?', const=1, default='',
+	                    help='Bundle mask(s)')
+	parser.add_argument('-box_size_max', nargs='?',  const=1, default='',
+	                    help='Maximum box size')                        
+	args = parser.parse_args()
+
+	box_size_max = np.asarray(args.box_size_max, dtype='float64')
+	box_size = np.arange(1, box_size_max) 
+
+	#Write results on a file
+	results = 'csv/output_FiberStats.csv'
+	metrics = ['FD']
+	first_row = ['tract_name']
+	first_row[1:len(metrics)] = metrics
+	with open(results, 'a') as csvFile:
+		writer = csv.writer(csvFile)
+		writer.writerow(first_row)
+
+	with open('tract_name_list.txt') as f:
+		tract_name_list = f.read().splitlines()
+
+	results_matrix = np.zeros((len(tract_name_list), len(metrics)))
+	
+	for t, tract_name in enumerate(tract_name_list):
+		fname = '%s/%s.nii.gz' %(args.masks, tract_name)
+		vol_mask = nib.load(fname).get_data()
+		fractal_dimension, box_size, counts = compute_fractal_dimension(vol_mask,
+                                                                    n_steps=None,
+                                                                    box_size_max=None,
+                                                                    box_size=box_size,
+                                                                    verbose=False)
+		print("Fractal dimension of %s = %s" %(tract_name, fractal_dimension))
+		results_matrix[t] = [fractal_dimension] 
+		row = [tract_name]
+		row[1:len(metrics)] = np.float16(results_matrix[t])
+		with open(results, 'a') as csvFile:
+			writer = csv.writer(csvFile)
+			writer.writerow(row)
+
+	sys.exit()    
